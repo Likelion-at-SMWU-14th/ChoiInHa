3f438ba1b646bd375cc6f566f00d64092b5110d3
최인하 <bella020207@naver.com>
2026-04-12 20:27:53 +0900

    Feat: Admin 커스터마이징

--- django/week01/gallery/admin.py
+++ django/week01/gallery/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image, ImageComment
 
 # Register your models here.
-admin.site.register(Image)
+
+class ImageCommentInline(admin.TabularInline):
+    model = ImageComment
+    extra = 0
+    min_num = 1
+    max_num = 5
+    verbose_name = '댓글'
+    verbose_name_plural = '댓글들'
+@admin.register(Image)
+class ImageModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'content', 'created_at']
+    list_editable = ['content']
+    list_filter = ['created_at']
+    search_fields = ['id', 'content']
+    search_help_text = '이미지 번호와 내용을 검색할 수 있습니다.'
+    inlines = [ImageCommentInline]
+    actions = ['report']
+
+    def report(self, request, queryset):
+        for item in queryset:
+            item.content = '운영규칙 위반 이미지 삭제'
+            item.save()
--- django/week01/gallery/models.py
+++ django/week01/gallery/models.py
@@ -5,3 +5,8 @@ class Image(models.Model):
     image = models.ImageField(verbose_name='이미지')
     content = models.TextField(verbose_name='내용')
     created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+
+class ImageComment(models.Model):
+    image = models.ForeignKey(Image, verbose_name='이미지', on_delete=models.CASCADE)
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
