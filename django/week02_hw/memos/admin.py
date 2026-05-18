7bcd6354fb243b202c555f86f167170d49dda22f
최인하 <bella020207@naver.com>
2026-05-18 22:41:21 +0900

    Feat: 2주차 과제 완료

--- django/week02_hw/memos/admin.py
+++ django/week02_hw/memos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Memo
+from .models import Comment
+
+admin.site.register(Memo)
+admin.site.register(Comment)
+
--- django/week02_hw/memos/models.py
+++ django/week02_hw/memos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
+class Memo(models.Model):
+    writer = models.ForeignKey(User, verbose_name='작성자', on_delete=models.CASCADE, null=True, blank=True)
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
+
+class Comment(models.Model):
+    memo = models.ForeignKey(Memo, verbose_name='메모', on_delete=models.CASCADE)
+    writer = models.ForeignKey(User, verbose_name='작성자', on_delete=models.CASCADE, null=True, blank=True)
+    content = models.TextField(verbose_name='내용')
+    created_at = models.DateTimeField(verbose_name='작성일', auto_now_add=True)
