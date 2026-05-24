1699c6df4a80099686c7b62702b7e223509ff5a6
최인하 <bella020207@naver.com>
2026-05-24 22:34:52 +0900

    Feat: 댓글 생성 및 수정

--- django/week03/posts/urls.py
+++ django/week03/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import post_form_view, post_list_view, post_model_form_view, post_detail_view, post_update_view, post_delete_view
+from .views import post_form_view, post_list_view, post_model_form_view, post_detail_view, post_update_view, post_delete_view, comment_create, comment_update
 
 app_name = 'posts'
 
@@ -10,4 +10,9 @@ urlpatterns = [
     path('<int:id>/', post_detail_view, name='post-detail'),
     path('<int:id>/update/', post_update_view, name='post-update'),
     path('<int:id>/delete/', post_delete_view, name='post-delete'),
+    path('<int:post_id>/', post_detail_view, name='post_detail'),
+    path('<int:post_id>/comments/create/', comment_create, name='comment_create'),
+    path('comments/<int:comment_id>/update/', comment_update, name='comment_update'),
+
+
 ]
