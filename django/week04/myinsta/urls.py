62fb19243fcb0b7f98befd3e1da383a1423cbf8e
최인하 <bella020207@naver.com>
2026-06-23 19:59:03 +0900

    Feat: @api_view+query param 계산기 구현

--- django/week04/myinsta/urls.py
+++ django/week04/myinsta/urls.py
@@ -20,10 +20,13 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from posts.views_cal import calculator_query
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls', namespace='posts')),
+    path('calculator/', calculator_query),
 ]
 
 if settings.DEBUG:
