366704063702a4f37d4ef89cf586ea10da0f5ca4
최인하 <bella020207@naver.com>
2026-07-25 19:25:45 -0700

    Feat:Post 생성 기능 구현

---
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/service/PostService.java
@@ -0,0 +1,19 @@
+package com.likelion.seminar.post.service;
+
+
+import com.likelion.seminar.post.dto.postDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final List<postDTO> postDTOList;
+
+    public void createPost(postDTO postDTO) {
+        this.postDTOList.add(postDTO);
+    }
+}
