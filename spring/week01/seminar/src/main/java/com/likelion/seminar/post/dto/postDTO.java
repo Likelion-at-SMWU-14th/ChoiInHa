89c1c7bcbea8c5e277ccc66e4dbcf32ed66d390b
최인하 <bella020207@naver.com>
2026-07-25 18:56:28 -0700

    Feat:PostDTO 정의

---
+++ spring/week01/seminar/src/main/java/com/likelion/seminar/post/dto/postDTO.java
@@ -0,0 +1,14 @@
+package com.likelion.seminar.post.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class postDTO {
+
+    private String title;
+    private String content;
+    private String writer;
+}
