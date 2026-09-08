df3e0c3e8f5d131f8a60a86704ac358519badeb2
최인하 <bella020207@naver.com>
2026-09-08 20:21:06 +0900

    Feat:다대 매핑 실습

--- spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Product.java
+++ spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Product.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
@@ -36,6 +39,10 @@ public class Product extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<Producer> producers = new ArrayList<>();
 
 
 }
