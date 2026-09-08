dcbef91264ecc937511f3e2b1803182859d27f58
최인하 <bella020207@naver.com>
2026-09-08 20:03:52 +0900

    Feat:일대다,다대일 매핑 실습

--- spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Product.java
+++ spring/week06/seminar/src/main/java/com/likelion/seminar/entity/Product.java
@@ -29,5 +29,13 @@ public class Product extends BaseEntity {
     @ToString.Exclude
     private ProductDetail productDetail;
 
+    @ManyToOne
+    @JoinColumn(name = "provider_id")
+    private Provider provider;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
 
 }
