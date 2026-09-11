c93920d6113db57fe7809a4a4698778b13832c28
최인하 <bella020207@naver.com>
2026-09-11 15:06:08 +0900

    Feat: comment test 완료

--- spring/week06/hw/src/main/java/com/likelion/hw/entity/User.java
+++ spring/week06/hw/src/main/java/com/likelion/hw/entity/User.java
@@ -30,7 +30,8 @@ public class User extends BaseEntity {
     @OneToMany(mappedBy = "user")
     private List<Post> posts = new ArrayList<>();
 
-    @OneToMany(mappedBy = "post")
+    // User.java
+    @OneToMany(mappedBy = "user")
     private List<Comment> comments = new ArrayList<>();
 
 
